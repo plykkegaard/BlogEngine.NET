94e31d9e36039a56207d3a6e54614306c2b9fce5
plyk <plyk@lemu.dk>
2026-07-18 09:26:45 +0200

    Add XML docs to IFileManagerRepository interface
    Added <summary>, <remarks>, <param>, and <returns> XML documentation comments to the IFileManagerRepository interface and its Find method to clarify their purpose and usage.

--- BlogEngine/BlogEngine.Core/Data/Contracts/IFileManagerRepository.cs
+++ BlogEngine/BlogEngine.Core/Data/Contracts/IFileManagerRepository.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 
 namespace BlogEngine.Core.Data.Contracts
 {
+    /// <summary>
+    /// Interface for FileManagerRepository
+    /// </summary>
+    /// <remarks>
+    /// This interface defines the contract for a file manager repository, which is responsible for managing file instances.
+    /// </remarks>
     public interface IFileManagerRepository
     {
+        /// <summary>
+        /// Finds file instances based on the specified parameters.
+        /// </summary>
+        /// <param name="take"></param>
+        /// <param name="skip"></param>
+        /// <param name="path"></param>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// The method returns an enumerable collection of file instances that match the specified criteria.
+        /// </remarks>
         IEnumerable<FileInstance> Find(int take = 10, int skip = 0, string path = "", string order = "");
     }
 }
